6c4b0a2eec26c6b90bcbf8aad5936d90995832ae
k8 <katesterchi@gmail.com>
2023-07-26 16:56:37 -0400

    added image to README, continued trying to solve display issues by adding event listener for form submission, changed .then to async with tutor, fixed query parameters with tutor

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,14 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
+    console.log("DOMContentLoaded event fired!");
     const weatherForm = document.getElementById("weatherForm");
     const cityInput = document.getElementById("cityInput");
     const weatherForecastDiv = document.getElementById("weatherForecast");
+    const fiveDayForecastDiv = document.getElementById("fiveDayForecast");
     const searchHistoryList = document.getElementById("searchHistory");
     const clearHistoryButton = document.getElementById("clearHistoryButton");
     const WeatherPsychicAppKey = 'd72fd408c6d82ddc85880d6e8ae64a2b'; 
-  
-       // Function to fetch weather data from the OpenWeather API
+
+
+  // Function to fetch weather data from the OpenWeather API
   function fetchWeatherData(cityName) {
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${WeatherPsychicAppKey}`;
+    console.log("Fetching weather data for:", cityName);
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${WeatherPsychicAppKey}&units=imperial`;
 
     fetch(apiUrl)
       .then(response => {
@@ -39,21 +43,112 @@ document.addEventListener("DOMContentLoaded", function () {
           <h3>Weather in ${city}</h3>
           <p>Date: ${formattedDate}</p>
           <p>Description: ${weatherDescription}</p>
-          <p>Temperature: ${temperature} °C</p>
+          <p>Temperature: ${temperature} °F</p>
           <p>Humidity: ${humidity}%</p>
           <img src="${iconUrl}" alt="Weather Icon">
         `;
+
+        // Fetch the 5-day weather forecast for the searched city
+        fetchFiveDayForecast(cityName);
+
+        // Only add the searched city to the search history list if it's not already in localStorage
+        if (!isCityInSearchHistory(cityName)) {
+          addToSearchHistory(cityName);
+        }
+      })
+      .catch(error => {
+        console.error("Error fetching weather data:", error);
+        weatherForecastDiv.textContent = "Error fetching weather data.";
+      });
+  }
+
   
-          // Only add the searched city to the search history list if it's not already in localStorage
-          if (!isCityInSearchHistory(cityName)) {
-            addToSearchHistory(cityName);
-          }
-        })
-        .catch(error => {
-          console.error("Error fetching weather data:", error);
-          weatherForecastDiv.textContent = "Error fetching weather data.";
+  // Function to fetch 5-day weather forecast data from the OpenWeather API
+  async function fetchFiveDayForecast(cityName) {
+    const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${WeatherPsychicAppKey}&units=imperial`;
+
+   const response = await fetch(apiUrl)
+        if (!response.ok) {
+          throw new Error("City not found. Please check your spelling and try again.");
+        }
+       const data = await response.json();
+
+        displayFiveDayForecast(data);
+    
+  }
+//   // Function to fetch 5-day weather forecast data from the OpenWeather API
+//   function fetchFiveDayForecast(cityName) {
+//     const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${WeatherPsychicAppKey}&units=imperial`;
+
+//     fetch(apiUrl)
+//       .then(response => {
+//         if (!response.ok) {
+//           throw new Error("City not found. Please check your spelling and try again.");
+//         }
+//         return response.json();
+//       })
+//       .then(data => {
+//         console.log("5-day forecast data fetched successfully:", data);
+//         displayFiveDayForecast(data);
+//       })
+//       .catch(error => {
+//         console.error("Error fetching 5-day forecast data:", error);
+//         fiveDayForecastDiv.textContent = error.message;
+//       });
+//   }
+  // Function to display the 5-day weather forecast
+  function displayFiveDayForecast(data) {
+    // Clear the previous 5-day forecast data
+    fiveDayForecastDiv.innerHTML = "";
+
+    // Get the list of weather data for the next 5 days (every 3 hours)
+    const forecastList = data.list;
+
+    // Create an array to store 5-day weather data
+    const fiveDayWeather = [];
+
+    // Loop through the forecast list and group weather data by date
+    for (const forecast of forecastList) {
+      const date = forecast.dt_txt.split(" ")[0];
+      const existingDay = fiveDayWeather.find(item => item.date === date);
+
+      if (!existingDay) {
+        fiveDayWeather.push({
+          date: date,
+          minTemp: forecast.main.temp_min,
+          maxTemp: forecast.main.temp_max,
+          weatherDescription: forecast.weather[0].description,
+          iconCode: forecast.weather[0].icon,
         });
+      } else {
+        if (forecast.main.temp_min < existingDay.minTemp) {
+          existingDay.minTemp = forecast.main.temp_min;
+        }
+        if (forecast.main.temp_max > existingDay.maxTemp) {
+          existingDay.maxTemp = forecast.main.temp_max;
+        }
+      }
     }
+
+    // Loop through the fiveDayWeather array and display the weather information
+    // for of loop comes from python 
+    for (const day of fiveDayWeather) {
+      const iconUrl = `https://openweathermap.org/img/w/${day.iconCode}.png`;
+
+      const dayDiv = document.createElement("div");
+      dayDiv.classList.add("day-forecast");
+
+      dayDiv.innerHTML = `
+        <p>Date: ${day.date}</p>
+        <p>Description: ${day.weatherDescription}</p>
+        <p>
+        <p>Max Temperature: ${day.maxTemp.toFixed(2)} °F</p>
+        <img src="${iconUrl}" alt="Weather Icon">
+      `;
+
+      fiveDayForecastDiv.appendChild(dayDiv);
+    }
+  }
   
     // Function to add the searched city to the search history list and localStorage
     function addToSearchHistory(cityName) {
@@ -71,23 +166,26 @@ document.addEventListener("DOMContentLoaded", function () {
   
       // Save the search history to localStorage
       const searchHistory = getSearchHistory();
-      searchHistory.push(cityName);
+      if (searchHistory.indexOf(cityName) === -1){
+        searchHistory.push(cityName);
+      }
+      
       localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
     }
   
-    // Function to get the search history from localStorage
+// Function to get the search history from localStorage
     function getSearchHistory() {
       const searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
       return searchHistory;
     }
   
-    // Function to check if a city is already in the search history
+ // Function to check if a city is already in the search history
     function isCityInSearchHistory(cityName) {
       const searchHistory = getSearchHistory();
       return searchHistory.includes(cityName);
     }
   
-    // Function to display the search history on page load
+ // Function to display the search history on page load
     function displaySearchHistory() {
       const searchHistory = getSearchHistory();
   
@@ -102,35 +200,30 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     }
 
-      // Function to clear the search history from localStorage and the list
+ // Function to clear the search history from localStorage and the list
   function clearSearchHistory() {
     localStorage.removeItem("searchHistory");
     searchHistoryList.innerHTML = "";
   }
+// Event listener for the form submission
+weatherForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const cityName = cityInput.value;
+    console.log("Form submitted with city:", cityName);
+    fetchWeatherData(cityName);
+  });
 
-  
-    // Add an event listener for form submission
-    weatherForm.addEventListener("submit", function (event) {
-      event.preventDefault(); // Prevent form submission from refreshing the page
-  
-      // Get the user input (city name)
-      const cityNameInput = cityInput.value.trim();
-  
-      if (cityNameInput === "") {
-        // Show an error message if the city name is empty
-        console.log("Please enter a city name.");
-        weatherForecastDiv.textContent = "Please enter a city name.";
-      } else {
-        // Fetch weather data for the searched city
-        fetchWeatherData(cityNameInput);
-      }
-    });
-
-     // Add an event listener for the "Clear Search History" button
+  // Add an event listener for the "Clear Search History" button
   clearHistoryButton.addEventListener("click", function () {
     clearSearchHistory();
   });
-  
-    // Display the search history on page load
-    displaySearchHistory();
-  });
+
+  // Display the search history on page load
+  displaySearchHistory();
+})
+
+
+
+
+
+
--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# weather-psychic
+# Weather Psychic
+
+
+## Description
 An application that displays current and future weather conditions of a city chosen by the user.
+
+Check out my weather application [here!](https://k8sterchi.github.io/weather-psychic/)
+
+
+## User Story
+As a traveler, I want to see the weather outlook for multiple cities SO THAT I can plan a trip accordingly.
+
+
+## Acceptance Criteria
+Given dashboard with form inputs:
+
+WHEN I search for a city, 
+
+THEN I am presented with current and future conditions for that city and that city is added to local storage. 
+
+WHEN I view current conditions for that city, 
+
+THEN I am presented with the city name, the date, an icon representation of the weather, the temperature, the humidity, and the wind speed. 
+
+WHEN I view future conditions for that city,
+
+THEN I am presented with a 5-day forecast with date, weather icon, temperature, wind speed, and humidity.
+
+WHEN I click on a city in the search history,
+
+THEN I am presented with current and future conditions for that city. 
+
+![Preview of application](./Assets/images/WeatherApp_Preview.png)
+
+## Resources:
+
+- I used the [openWeather](https://openweathermap.org/) API to retrieve weather data.
+
+
+- Geocoding: for how to retrieve geographical coordinates using a given city's name, [click here.](https://openweathermap.org/api/geocoding-api)
+
+- Tutoring / pair programming with Dane Shrewsbury, incredibly helpful. 
+
+- [ChatGPT](https://openai.com/chatgpt) continues to help me write succinct and dynamic code. 
+
+- I continue to refer to [markdown best practices](https://www.markdownguide.org/basic-syntax) in order to develop best practices.
+
+- [This full-stack blog](https://coding-boot-camp.github.io/full-stack/apis/how-to-use-api-keys) on how to use API keys was informative.
+
+- It is your responsibility to include a note in the README section of your repo specifying code source and its location within your repo.
